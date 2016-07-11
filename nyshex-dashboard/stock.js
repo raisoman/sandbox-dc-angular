40280d53daac3f2ad70164ecf70dab78906c7428
Nicolas Guilbert <nicolas@ange.dk>
2016-07-11 13:04:14 +0200

    Range chart updates multiple focus charts

--- nyshex-dashboard/stock.js
+++ nyshex-dashboard/stock.js
@@ -61,6 +61,55 @@ angular.module("app", ["angularDc"])
         });
         $scope.containerDestinationGroup = $scope.containerDestination.group();
 
+        $scope.volumeChartPostSetup = function(chart, options) {
+            chart.x(d3.time.scale().domain([$scope.byWeek.bottom(1)[0].dd, $scope.byWeek.top(1)[0].dd]))
+            chart.yAxis().ticks(0);
+
+            var rangesEqual = function (range1, range2) {
+                if (!range1 && !range2) {
+                    return true;
+                } else if (!range1 || !range2) {
+                    return false;
+                } else if (range1.length === 0 && range2.length === 0) {
+                    return true;
+                } else if (range1[0].valueOf() === range2[0].valueOf() &&
+                    range1[1].valueOf() === range2[1].valueOf()) {
+                    return true;
+                }
+                return false;
+            }
+
+            //Extend the range selector chart with the ability to update multiple charts
+            chart.addFocusChart = function (newFocusChart) {
+                if (!arguments.length) {
+                    return this._focusCharts;
+                }
+                if (this._focusCharts) {
+                    this._focusCharts.push(newFocusChart)
+                } else {
+                    this._focusCharts = [newFocusChart];
+                }
+                var _this = this;
+                this.on('filtered', function (range_chart) {
+                    if (!range_chart.filter()) {
+                        dc.events.trigger(function () {
+                            _this._focusCharts.forEach(function(focus_chart) {
+                                focus_chart.x().domain(focus_chart.xOriginalDomain());
+                            });
+                        });
+                    } else _this._focusCharts.forEach(function(focus_chart) {
+                        if (!rangesEqual(range_chart.filter(), focus_chart.filter())) {
+                            dc.events.trigger(function () {
+                                focus_chart.focus(range_chart.filter());
+                            });
+                        }
+                    });
+                });
+                return this;
+            };
+
+        }
+
         $scope.compositePostSetup = function(chart, options){
             chart.compose([
                 dc.barChart(chart)
@@ -80,6 +129,7 @@ angular.module("app", ["angularDc"])
             ]);
             chart.x(d3.time.scale().domain([$scope.byWeek.bottom(1)[0].dd, $scope.byWeek.top(1)[0].dd]))
             chart.legend(dc.legend().x(70).y(10).itemHeight(13).gap(5))
+            $scope.volumeChart.addFocusChart(chart);
         }
 
         $scope.containerTypePostSetupChart = function(c) {
@@ -92,19 +142,17 @@ angular.module("app", ["angularDc"])
             .xAxis().ticks(4);
         }
 
-        $scope.volumeChartPostSetup = function(chart, options) {
-            chart.x(d3.time.scale().domain([$scope.byWeek.bottom(1)[0].dd, $scope.byWeek.top(1)[0].dd]))
-            chart.yAxis().ticks(0);
-        }
-
         $scope.carrierAllocationPostSetup = function(chart, options) {
             chart.x(d3.time.scale().domain([$scope.byWeek.bottom(1)[0].dd, $scope.byWeek.top(1)[0].dd]))
+            var barChart = dc.barChart(chart)
+                    .group($scope.volumeByWeekGroup)
+
             var originStackComponents = {};
             var origins = d3.map(data, function(d){return d.originCode;}).keys();
             var originGroup0 = $scope.byWeek.group().reduceSum(function (d) {
                 return d.originCode == origins[0]?  d.teuQuantity : 0;
             });
-            chart.group(originGroup0);
+            barChart.group(originGroup0);
             for(var i = 1; i <origins.length; ++i) {
                 var originCode = origins[i];
                 //get the actual value of originCode instead of the closure variable
@@ -113,8 +161,14 @@ angular.module("app", ["angularDc"])
                         return d.originCode == originCodeInstance?  d.teuQuantity : 0;
                     });
                 })(originCode);
-                chart.stack(originStackComponent);
+                barChart.stack(originStackComponent);
             };
+
+            chart.compose([
+                barChart
+                ]);
+
+            $scope.volumeChart.addFocusChart(chart);
         }
 
         $scope.printDiv = function(divName) {
